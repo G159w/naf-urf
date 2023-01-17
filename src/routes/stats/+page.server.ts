bfd171843a4bfc2353cb0ffb6a4a52f212a66878
Henri BAUDESSON <henri.baudesson@polyconseil.fr>
2023-01-18 00:05:29 +0100

    global stats v1

--- src/routes/stats/+page.server.ts
+++ src/routes/stats/+page.server.ts
@@ -24,12 +24,103 @@ export const load = (async () => {
 			tripleKills: true,
 			quadraKills: true,
 			pentaKills: true
+		},
+		_max: {
+			kills: true,
+			deaths: true,
+			assists: true,
+			damage: true
+		},
+		_min: {
+			kills: true,
+			deaths: true,
+			assists: true
 		}
+	});
+
+	const gamesWon = await prisma.game.count({
+		where: {
+			players: {
+				some: {
+					user: {
+						id: { gt: 0 }
+					},
+					isWin: {
+						equals: true
+					}
+				}
+			}
+		}
+	});
+
+	const totalGames = await prisma.game.count({
+		where: {
+			players: {
+				some: {
+					user: {
+						id: { gt: 0 }
+					}
+				}
+			}
+		}
+	});
+
+	console.log(gamesWon, totalGames);
+
+	const champion = await prisma.champion.findMany({
+		include: {
+			_count: {
+				select: {
+					players: {
+						where: {
+							user: { id: { gt: 0 } }
+						}
+					}
+				}
+			}
+		},
+		orderBy: [
+			{
+				players: {
+					_count: 'desc'
+				}
+			}
+		],
+		take: 1
+	});
+
+	const championWin = await prisma.champion.findMany({
+		include: {
+			_count: {
+				select: {
+					players: {
+						where: {
+							user: { id: { gt: 0 } },
+							isWin: true
+						}
+					}
+				}
+			}
+		},
+		orderBy: [
+			{
+				players: {
+					_count: 'desc'
+				}
+			}
+		],
+		take: 1
 	});
 
 	return {
 		users: await prisma.user.findMany({ include: { _count: { select: { gameStats: true } } } }),
-		avg: avg._avg
+		avg: avg._avg,
+		max: {
+			...avg._max,
+			champion: champion[0],
+			championWin: championWin[0]
+		},
+		wr: (gamesWon / totalGames) * 100
 	};
 }) satisfies PageServerLoad;
 
--- src/routes/stats/+page.svelte
+++ src/routes/stats/+page.svelte
@@ -5,6 +5,7 @@
 	import { fade } from 'svelte/transition';
 	import CreateUser from '$lib/component/CreateUser.svelte';
 	import { UserPlus } from 'lucide-svelte';
+	import { getChampionName } from '$lib/utils';
 
 	export let data: PageData;
 
@@ -35,51 +36,110 @@
 
 	<hr class="w-full" />
 	<hr />
-	<div class="flex flex-col gap-4 w-full">
-		<h2>Moyennes:</h2>
+	<div class="w-full flex flex-row justify-between">
 		<div class="flex flex-col gap-4">
-			<div class="flex flex-col">
-				<div class="font-bold">K/D/A:</div>
-				<div>
-					{data.avg.kills?.toFixed(1)}/{data.avg.deaths?.toFixed(1)}/{data.avg.assists?.toFixed(1)}
+			<h2>Moyennes:</h2>
+			<div class="flex flex-col gap-4">
+				<div class="flex flex-col">
+					<div class="font-bold">K/D/A:</div>
+					<div>
+						{data.avg.kills?.toFixed(1)}/{data.avg.deaths?.toFixed(1)}/{data.avg.assists?.toFixed(
+							1
+						)}
+					</div>
+				</div>
+				<div class="flex flex-col">
+					<div class="font-bold">Dégâts:</div>
+					<div>
+						{data.avg.damage?.toLocaleString()}
+					</div>
+				</div>
+				<div class="flex flex-col">
+					<div class="font-bold">Gold:</div>
+					<div>
+						{data.avg.goldEarned?.toLocaleString()}
+					</div>
+				</div>
+				<div class="flex flex-col">
+					<div class="font-bold">Double kills:</div>
+					<div>
+						{((data.avg.doubleKills || 0) * 100).toFixed(2)} %
+					</div>
+				</div>
+				<div class="flex flex-col">
+					<div class="font-bold">Triple kills:</div>
+					<div>
+						{((data.avg.tripleKills || 0) * 100).toFixed(2)} %
+					</div>
+				</div>
+				<div class="flex flex-col">
+					<div class="font-bold">Quadrakill:</div>
+					<div>
+						{((data.avg.quadraKills || 0) * 100).toFixed(2)} %
+					</div>
+				</div>
+				<div class="flex flex-col">
+					<div class="font-bold">Pentakills:</div>
+					<div>
+						{((data.avg.pentaKills || 0) * 100).toFixed(2)} %
+					</div>
 				</div>
 			</div>
-			<div class="flex flex-col">
-				<div class="font-bold">Dégâts:</div>
-				<div>
-					{data.avg.damage?.toLocaleString()}
+		</div>
+		<div class="flex flex-col gap-4 ">
+			<div class="flex flex-col gap-4">
+				<div class="flex flex-col">
+					<div class="font-bold">Win Rate:</div>
+					<div>
+						{data.wr.toFixed(2)} %
+					</div>
+				</div>
+				<div class="flex flex-col">
+					<div class="font-bold">Max kills:</div>
+					<div>
+						{data.max.kills?.toLocaleString()}
+					</div>
+				</div>
+				<div class="flex flex-col">
+					<div class="font-bold">Max deaths:</div>
+					<div>
+						{data.max.deaths?.toLocaleString()}
+					</div>
+				</div>
+				<div class="flex flex-col">
+					<div class="font-bold">Max assists:</div>
+					<div>
+						{data.max.assists?.toLocaleString()}
+					</div>
+				</div>
+				<div class="flex flex-col">
+					<div class="font-bold">Max dégâts:</div>
+					<div>
+						{data.max.damage?.toLocaleString()}
+					</div>
+				</div>
+				<div class="flex flex-col">
+					<div class="font-bold">Champions le plus joué:</div>
+					<div>
+						{data.max.champion.name} ({data.max.champion._count.players} picks)
+					</div>
+				</div>
+				<div class="flex flex-col">
+					<div class="font-bold">Champions qui a le plus gagné:</div>
+					<div>
+						{data.max.championWin.name} ({data.max.championWin._count.players} wins)
+					</div>
 				</div>
 			</div>
-			<div class="flex flex-col">
-				<div class="font-bold">Gold:</div>
-				<div>
-					{data.avg.goldEarned?.toLocaleString()}
-				</div>
-			</div>
-			<div class="flex flex-col">
-				<div class="font-bold">Double kills:</div>
-				<div>
-					{((data.avg.doubleKills || 0) * 100).toFixed(2)} %
-				</div>
-			</div>
-			<div class="flex flex-col">
-				<div class="font-bold">Triple kills:</div>
-				<div>
-					{((data.avg.tripleKills || 0) * 100).toFixed(2)} %
-				</div>
-			</div>
-			<div class="flex flex-col">
-				<div class="font-bold">Quadrakill:</div>
-				<div>
-					{((data.avg.quadraKills || 0) * 100).toFixed(2)} %
-				</div>
-			</div>
-			<div class="flex flex-col">
-				<div class="font-bold">Pentakills:</div>
-				<div>
-					{((data.avg.pentaKills || 0) * 100).toFixed(2)} %
-				</div>
-			</div>
+		</div>
+		<div>
+			<img
+				class="basis-1/12s"
+				alt={data.max.champion.name}
+				src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${getChampionName(
+					data.max.champion.name
+				)}_0.jpg`}
+			/>
 		</div>
 	</div>
 </section>
